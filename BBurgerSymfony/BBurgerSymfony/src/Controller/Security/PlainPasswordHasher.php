97c5a05bede8e750bb0fbcbf1885ae54398d0b76
Isaacdackey <isaacdackeyuniv@gmail.com>
2025-12-29 07:30:28 +0000

    FIX: Add Security root

---
+++ BBurgerSymfony/BBurgerSymfony/src/Controller/Security/PlainPasswordHasher.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Component\PasswordHasher\Exception\InvalidPasswordException;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherAwareInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+
+class PlainPasswordHasher implements PasswordHasherInterface
+{
+    public function hash(string $plainPassword): string
+    {
+        return $plainPassword;
+    }
+
+    public function verify(string $hashedPassword, string $plainPassword): bool
+    {
+        return $plainPassword === $hashedPassword;
+    }
+
+    public function needsRehash(string $hashedPassword): bool
+    {
+        return false;
+    }
+}
